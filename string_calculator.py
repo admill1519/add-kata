9ee389ed75abfa1ce28ebccfacb137529a16c93e
“admill1519” <admill1519@gmail.com>
2025-01-16 14:26:13 +0530

    Added functionality to return single numeric value as is

--- string_calculator.py
+++ string_calculator.py
@@ -3,3 +3,5 @@ class StringCalculator:
         if isinstance(numbers, str):
             if not numbers.strip():
                 return 0
+            elif len(numbers.strip()) == 1 and numbers.strip().isnumeric():
+                return int(numbers)
