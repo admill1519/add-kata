fe3e2ef2bf20abf1ad1db14e33bc03fc63e6f95a
“admill1519” <admill1519@gmail.com>
2025-01-16 14:32:55 +0530

    added testcase to check if code can handle 2 or more , seperated string value

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -11,3 +11,6 @@ def test_empty_string_returns_zero(calculator):
 
 def test_single_number_returns_value(calculator):
     assert calculator.add("1") == 1
+
+def test_two_or_more_numbers_return_sum(calculator):
+    assert calculator.add("1,2,3,4") == 10
